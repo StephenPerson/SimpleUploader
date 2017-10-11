abfefbb72772401c98bf359430eca35fea0b523c
StephenPerson <stephen.person.12@cnu.edu>
2017-10-10 22:21:07 -0400

    laan

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,6 @@ set :forward_agent, true     # SSH forward_agent.
 # shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
 set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
-set :shared_paths, fetch(:shared_paths, []).push('shared/sockets', 'shared/pids')
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
 ruby_version = File.read('.ruby-version')
